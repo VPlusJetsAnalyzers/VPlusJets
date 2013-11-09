28926e6365078e8b7cc015f24ea84fe0df14bbfe
Phillip <pdudero@gmail.com>
2013-11-08 18:01:13 -0600

    take release directory as first argument

--- test/Limits/batchit.sh
+++ test/Limits/batchit.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-#SRCDIR=/uscms/home/pdudero/work/CMSSW_5_3_3_patch3/src/ElectroWeakAnalysis/VPlusJets/test/Limits/
-SRCDIR=/uscms/home/pdudero/work/CMSSW_6_1_1/src/ElectroWeakAnalysis/VPlusJets/test/Limits/
+SRCDIR=$1
+shift
 cd $SRCDIR
 #export SCRAM_ARCH=slc5_amd64_gcc462
 export SCRAM_ARCH=slc5_amd64_gcc472
 eval `scram runtime -sh`
+#if [ -e pyroot_logon.py ]
+#then
+#    python pyroot_logon.py
+#fi
 cd -
 echo "$*"
 `$*`
