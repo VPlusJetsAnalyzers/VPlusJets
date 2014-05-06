d96ba63cc49729e9b176ac44d8818e52f96e6721
Phillip <pdudero@gmail.com>
2014-05-06 16:05:34 -0500

    updates to poly coeffs

--- test/TGC/makeTreeFromText.C
+++ test/TGC/makeTreeFromText.C
@@ -1,31 +1,32 @@
 
 void makeTreeFromText() {
 
-   makeTreeFromText("params-ww-kappa-lambda-cteq6l1-fitted-withCuts.list", 
+   // Note: now *p2* missing for both WW and WZ functions
+
+
+   makeTreeFromText("params-ww-kappa-lambda-cteq6l1-fitted-test.list", 
 		    "ww_root_kappa-lambda.root",
-		    "lambda/F:dkappa:p0:p1:p2:p3:p4:p5:chi2");
+		    "lambda/F:dkappa:p0:p1:p3:p4:p5:chi2");
 
-   makeTreeFromText("params-ww-lambda-g1z-cteq6l1-fitted-withCuts.list", 
+   makeTreeFromText("params-ww-lambda-g1z-cteq6l1-fitted-test.list", 
 		    "ww_root_lambda-g1.root",
-		    "lambda/F:dg1:p0:p1:p2:p3:p4:p5:chi2");
+		    "lambda/F:dg1:p0:p1:p3:p4:p5:chi2");
 
-   makeTreeFromText("params-ww-kappa-g1z-cteq6l1-fitted-withCuts.list",
+   makeTreeFromText("params-ww-kappa-g1z-cteq6l1-fitted-test.list",
 		    "ww_root_kappa-g1.root",
-		    "dkappa/F:dg1:p0:p1:p2:p3:p4:p5:chi2");
+		    "dkappa/F:dg1:p0:p1:p3:p4:p5:chi2");
 
-   // Note: p4 missing for WZ functions
-
-   makeTreeFromText("params-wz-kappa-lambda-cteq6l1-fitted-withCuts.list", 
+   makeTreeFromText("params-wz-kappa-lambda-cteq6l1-fitted-test.list", 
 		    "wz_root_kappa-lambda.root",
-		    "lambda/F:dkappa:p0:p1:p2:p3:p5:p6:chi2");
+		    "lambda/F:dkappa:p0:p1:p3:p4:p5:p6:chi2");
 
-   makeTreeFromText("params-wz-lambda-g1z-cteq6l1-fitted-withCuts.list", 
+   makeTreeFromText("params-wz-lambda-g1z-cteq6l1-fitted-test.list", 
 		    "wz_root_lambda-g1.root",
-		    "lambda/F:dg1:p0:p1:p2:p3:p5:p6:chi2");
+		    "lambda/F:dg1:p0:p1:p3:p4:p5:p6:chi2");
 
-   makeTreeFromText("params-wz-kappa-g1z-cteq6l1-fitted-withCuts.list",
+   makeTreeFromText("params-wz-kappa-g1z-cteq6l1-fitted-test.list",
 		    "wz_root_kappa-g1.root",
-		    "dkappa/F:dg1:p0:p1:p2:p3:p5:p6:chi2");
+		    "dkappa/F:dg1:p0:p1:p3:p4:p5:p6:chi2");
 }
 
 void makeTreeFromText(const char* inputFile,
