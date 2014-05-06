791eae6f572d6087cdc51e8528d19f0ef30c86a0
Phillip <pdudero@gmail.com>
2014-05-06 15:56:26 -0500

    make it work better

--- test/Limits/hwwcondormylimit.sh
+++ test/Limits/hwwcondormylimit.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-BASEDIR=/uscms/home/pdudero/data/HWW/HWWmvaOptFit130219
-SRCDIR=/uscms/home/pdudero/work/CMSSW_5_3_3_patch3/src/ElectroWeakAnalysis/VPlusJets/test/Limits/
-submit=condor_submit.txt
-executable=${SRCDIR}/hwwrunCombine.sh
+#BASEDIR=/uscms/home/pdudero/data/HWW/HWWmvaOptFit130219
+SRCDIR=/uscms/home/pdudero/work/CMSSW_6_1_2/src/ElectroWeakAnalysis/VPlusJets/test/Limits/
+
+#executable=${SRCDIR}/hwwrunCombine.sh
+executable=${SRCDIR}/batchit.sh
+cmbscript1=${SRCDIR}/combine_asymp.sh
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 <fileglob> ( e.g., datacard*.txt)"
+    exit
+fi
+
+if [ "$LOCALRT" == "" ]
+then
+    echo "ERROR: cms environment LOCALRT not set"
+    exit
+fi
+
+submit=submit.txt
 
 printheader() {
 cat >$submit <<EOF
@@ -10,24 +26,41 @@ Executable = $executable
 Universe = vanilla
 Requirements = Memory > 250 && FileSystemDomain=="fnal.gov" && Disk > 500000 && Arch=="X86_64"
 Should_Transfer_Files = YES
-transfer_input_files = $SRCDIR/ggHtable8tev.txt,$SRCDIR/twikiBRtable.txt,$SRCDIR/vbfHtable8tev.txt,$SRCDIR/jetbinerrtable.txt
 WhenToTransferOutput = ON_EXIT
+Notification = ERROR
 EOF
 }
 
-printargs() {
-    echo "Error = $BASEDIR/limit_M=${mH}.log" >>$submit
-    echo "Output = $BASEDIR/limit_M=${mH}.log" >>$submit
-    echo "Arguments = $rootf" >>$submit
+#transfer_input_files = $SRCDIR/ggHtable8tev.txt,$SRCDIR/twikiBRtable.txt,$SRCDIR/vbfHtable8tev.txt,$SRCDIR/jetbinerrtable.txt
+
+queuecards() {
+    echo "transfer_input_files = $xferlist" >>$submit
+    echo "Error = hwwlimit_${cfgtag}.log" >>$submit
+    echo "Output = hwwlimit_${cfgtag}.log" >>$submit
+    echo "Arguments = $arglist" >>$submit
     echo "Queue" >>$submit
 }
 
 printheader
-for rootf in $BASEDIR/*.root
-  do
-  mH=`echo $rootf | egrep -o "HWW[0-9]{3}" | egrep -o "[0-9]{3}"`
-  printargs
+
+for file in $*
+do
+  ROOTFILES=`grep -o "hwwinputdir.*root" $file | sort -u |xargs|sed 's# #,#g'`
+  ADDTLFILES=",$cmbscript1,$ROOTFILES"
+  xferlist="$file${ADDTLFILES}"
+  scriptbase=`basename $cmbscript1`
+  filebase=`basename $file`
+  arglist="$LOCALRT ln -s . hwwinputdir; ./$scriptbase $filebase"
+  base=`basename $file ${FILESUFFIX}`
+  cfgtag=${base##datacard_}
+  queuecards
 done
+
+# for rootf in $BASEDIR/*.root
+#   do
+#   mH=`echo $rootf | egrep -o "HWW[0-9]{3}" | egrep -o "[0-9]{3}"`
+#   printargs
+# done
 
 # do
 #   outputfile="explimvscutval-M=${masspt}_${channel}.tab"
