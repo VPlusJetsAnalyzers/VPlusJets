825465b1c9c7d5ebc15b90333f5f4eeab5ab8b4c
Phillip <pdudero@gmail.com>
2014-05-19 15:33:22 -0500

    new utilities for diboson, etc.

---
+++ test/Utilities/batchit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SRCDIR=$1
+shift
+cd $SRCDIR
+#export SCRAM_ARCH=slc5_amd64_gcc462
+export SCRAM_ARCH=slc5_amd64_gcc472
+eval `scram runtime -sh`
+#if [ -e pyroot_logon.py ]
+#then
+#    python pyroot_logon.py
+#fi
+cd -
+echo "$*"
+eval "$*"
